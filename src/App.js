3a78c6a8a5e695c3a36ef2782e045a7034898124
Ana <ana.bastos881@gmail.com>
2018-04-05 13:54:06 -0300

    Initial Commit 🤷🏻‍♀️

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from './pagedraw/header';
+import Main from './Main'
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+)
+
+export default App;
